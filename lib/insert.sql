31eaf22c7f0a6e3a14b1b3d4a4cac2aea33687df
Daniel Ran <danielr1735@gmail.com>
2018-07-01 19:07:16 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  values ("Mr. Chocolate", 6, M, "black", "Witty", 1),
+          ("Rowdy", null, null, null, null, null)
+          ("Tabitha", null, null, null, null, null)
+          ("Sergeant Brown", null, null, null, null, null)
+          ("Melissa", null, null, null, null, null)
+          ("Grinch", null, null, null, null, null)
+          ("Wendy", null, null, null, null, null)
