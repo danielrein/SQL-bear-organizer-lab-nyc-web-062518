63dc30e6395d4015bb220c26bb85be6273f7a48f
Daniel Ran <danielr1735@gmail.com>
2018-07-01 19:05:10 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bears (id int, name text, age int, gender char(1), color text, temperament text, alive boolean);
+CREATE TABLE bears (id int primary key, name text, age int, gender char(1), color text, temperament text, alive boolean);
