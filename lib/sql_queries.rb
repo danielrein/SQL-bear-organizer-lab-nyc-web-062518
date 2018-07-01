e60d5bdb1a500cdc3aae230e2d084be06a24bb68
Daniel Ran <danielr1735@gmail.com>
2018-07-01 19:15:31 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,6 +7,7 @@ end
 def selects_all_bears_names_and_orders_in_alphabetical_order
   'SELECT * FROM bears'
 binding.pry
+end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
